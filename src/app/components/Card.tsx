3613ac8d8850f4806134f4c1aefa1861d0b978de
sobia523 <syedasobia866@gmail.com>
2024-12-29 16:35:38 +0500

    updated code

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import Image from 'next/image';
 
+import React, { useEffect, useState } from 'react';
+import Image from 'next/image';
 
 interface propsType {
     title: string;
@@ -10,23 +10,46 @@ interface propsType {
 }
 
 const Card: React.FC<propsType> = ({ title, desc, img, tags }) => {
-    return(
-        <div className={`card ${window.innerWidth >= 640 ? 'card-sm' : ''}`} data-aos="zoom-in-up">
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        // Ye code sirf client-side pe chalega
+        const handleResize = () => {
+            setIsMobile(window.innerWidth < 640);
+        };
+
+        // Initial resize check
+        handleResize();
+
+        // Resize event listener add karte hain
+        window.addEventListener('resize', handleResize);
+
+        // Cleanup the event listener on component unmount
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    return (
+        <div className={`card ${isMobile ? '' : 'card-sm'}`} data-aos="zoom-in-up">
             <div>
-                <Image className="card-images card-images-sm" 
-                src={img}
-                width={350}
-                height={350}
-                alt={title}
+                <Image
+                    className="card-images card-images-sm"
+                    src={img}
+                    width={350}
+                    height={350}
+                    alt={title}
                 />
             </div>
             <div className="card-content">
                 <div className="card-title">{title}</div>
                 <div>{desc}</div>
                 <div>
-                    {tags.map((el) => (<div className='card-tags' key={el}>
-                        {el}
-                    </div>))}
+                    {tags.map((el) => (
+                        <div className="card-tags" key={el}>
+                            {el}
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
